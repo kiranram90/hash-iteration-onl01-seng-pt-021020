0540be71685e335758173d6453ac350964742fb1
kiran ram <kram@lions.molloy.edu>
2020-02-23 20:43:58 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,8 +8,8 @@
 
 def select_winner(passengers)
 
+passengers.each do |suite, passenger|
 winner = ""
-passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
     winner = name
   end
